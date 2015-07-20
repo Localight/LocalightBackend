780f5ab3b2b0a4163b2b85804edd4f9ad10a53cd
James Hall <greatwolf3d@gmail.com>
2015-07-20 15:46:48 -0700

    added a error handling service which can be used in anything.

--- routes/giftcards.js
+++ routes/giftcards.js
@@ -1,10 +1,33 @@
+'use strict';
+/**
+ *  Module Dependices
+ */
 var express = require('express'),
     router = express.Router(),
     mongoose = require('mongoose'),
-    giftcard = mongoose.model('giftcard');
+    GiftCard = mongoose.model('GiftCard');
 
 /* Giftcard routes. */
-router.get('/', function(req, res, next) {
+router.post('/giftcards', function(req, res, next) {
+   console.log('This the value of the request'+JSON.stringify(req.body));
+   // TODO: need to be sure that we are doign a little
+   //
+   var giftcard = new GiftCard({
+      //NOTE:why does giftcard have reference to a session token?
+      sessionToken:req.body.sessionToken,
+      message: req.body.message,
+      toId: req.body.toId,
+      fromId: req.body.fromId,
+      stripeOrderId: req.body.stripeOrderId,
+   });
+   giftcard.save(function(err){
+      if(err){
+         return res.status(400).send({
+
+         })
+      }
+   })
+
   res.send('Respond with giftcard stuff');
 });
 
