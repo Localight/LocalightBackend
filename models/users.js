99b39ca9ea2b9ca0083d61801473fb89ef712c7b
James Hall <greatwolf3d@gmail.com>
2015-07-20 16:24:29 -0700

    Added The User Model Schema

--- models/users.js
+++ models/users.js
@@ -1,6 +1,46 @@
-var mongoose = require('mongoose');
-var user = new mongoose.Schema({
-    //schema data
-});
+'use strict';
 
-mongoose.model('user', user);
+/**
+ * Module Dependencies
+ */
+var mongoose = require('mongoose'),
+    Schema = mongoose.Schema;
+
+var UserSchema = new Schema({
+    
+   firstName:{
+     type: String,
+     trim: true,
+     default: '',
+   },
+   lastName:{
+     type:String,
+     trime:true,
+     default:''
+   },
+   email:{
+     type:String,
+     trim:true,
+     default: '',
+     match: [/.+\@.+\..+/, 'Please fill a valid email address']
+  },
+   phone:{
+     type: String,
+     trim: true,
+     unique: 'testing error message',
+     match: [/^\s*(?:\+?(\d{1,3}))?[-. (]*(\d{3})[-. )]*(\d{3})[-. ]*(\d{4})(?: *x(\d+))?\s*$/, 'Please fill a valid ten-digit phone number'], // should match the format of a string phonenumber
+    required: 'Please fill in a mobile number'
+  },
+  password:{
+    type:String,
+    default:'',
+   },
+  salt:{
+   type:String
+   },
+  create:{
+    type:Date,
+    default:Date.now
+  }
+});
+mongoose.model('User', UserSchema);
