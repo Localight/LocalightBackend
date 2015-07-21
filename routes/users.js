309c0a21f42616fe1bcbec5c1623844096f58e94
Aaron <torch2424@hotmail.com>
2015-07-21 09:45:17 -0700

    Reorganized the backend route calls to make more sense

--- routes/users.js
+++ routes/users.js
@@ -8,21 +8,6 @@ router.post('/users/join', function(req, res, next) {
   //Logic goes here
 });
 
-/* Get a user */
-router.get('/users', function(req, res, next) {
-  //Logic goes here
-});
-
-/* update a user */
-router.put('/users', function(req, res, next) {
-  //Logic goes here
-});
-
-/* remove a user */
-router.delete('/users', function(req, res, next) {
-  //Logic goes here
-});
-
 /* login a user */
 router.post('/users/login', function(req, res, next) {
   //Logic goes here
@@ -35,6 +20,21 @@ router.post('/users/twilio', function(req, res, next) {
 
 /* reset a user */
 router.post('/users/reset', function(req, res, next) {
+  //Logic goes here
+});
+
+/* Get a user */
+router.get('/users', function(req, res, next) {
+  //Logic goes here
+});
+
+/* update a user */
+router.put('/users', function(req, res, next) {
+  //Logic goes here
+});
+
+/* remove a user */
+router.delete('/users', function(req, res, next) {
   //Logic goes here
 });
 
