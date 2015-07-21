77214d03965cc1007bdd2847f8d0dcd9f72d923a
Aaron <torch2424@hotmail.com>
2015-07-20 17:40:56 -0700

    Filled out the sessions schema

--- models/sessions.js
+++ models/sessions.js
@@ -1,6 +1,17 @@
 var mongoose = require('mongoose');
 var session = new mongoose.Schema({
-    //schema data
+    accountId: {
+        type: String,
+        require:'Please provide the id of the account'
+    },
+    type: {
+        type: String,
+        require: "Please provide the type of session"
+    },
+    token: {
+        type: String,
+        require: "Please provide the session token"
+    }
 });
 
 mongoose.model('session', session);
