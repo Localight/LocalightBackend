750edf97901f425ef8a04597e8e626273fbdb54e
Aaron <torch2424@hotmail.com>
2015-07-21 15:12:05 -0700

    Fixed the routes

--- routes/users.js
+++ routes/users.js
@@ -4,37 +4,37 @@ var express = require('express'),
     user = mongoose.model('user');
 
 /* Join a user */
-router.post('/users/join', function(req, res, next) {
+router.post('/join', function(req, res, next) {
   //Logic goes here
 });
 
 /* login a user */
-router.post('/users/login', function(req, res, next) {
+router.post('/login', function(req, res, next) {
   //Logic goes here
 });
 
 /* signup a user through twilio */
-router.post('/users/twilio', function(req, res, next) {
+router.post('/twilio', function(req, res, next) {
   //Logic goes here
 });
 
 /* reset a user */
-router.post('/users/reset', function(req, res, next) {
+router.post('/reset', function(req, res, next) {
   //Logic goes here
 });
 
 /* Get a user */
-router.get('/users', function(req, res, next) {
+router.get('/', function(req, res, next) {
   //Logic goes here
 });
 
 /* update a user */
-router.put('/users', function(req, res, next) {
+router.put('/', function(req, res, next) {
   //Logic goes here
 });
 
 /* remove a user */
-router.delete('/users', function(req, res, next) {
+router.delete('/', function(req, res, next) {
   //Logic goes here
 });
 
