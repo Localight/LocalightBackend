458add080358d645f87ee9344f5e9e869c5d4f7c
Aaron <torch2424@hotmail.com>
2015-07-21 10:23:14 -0700

    Created the Skeleton for the Location Routes

--- routes/locations.js
+++ routes/locations.js
@@ -3,9 +3,34 @@ var express = require('express'),
     mongoose = require('mongoose'),
     location = mongoose.model('location');
 
-/* Location routes */
-router.get('/', function(req, res, next) {
-  res.send('Respond with location stuff');
+/* Create a Location */
+router.post('/locations', function(req, res, next) {
+  //Logic goes here
+});
+
+/* Update a Location */
+router.put('/locations', function(req, res, next) {
+  //Logic goes here
+});
+
+/* Get a Location */
+router.get('/locations', function(req, res, next) {
+  //Logic goes here
+});
+
+/* Delete a Location */
+router.delete('/locations', function(req, res, next) {
+  //Logic goes here
+});
+
+/* Get a Location by id */
+router.get('/locations/:id', function(req, res, next) {
+  //Logic goes here
+});
+
+/* Make a purchase at a location */
+router.post('/locations/spend', function(req, res, next) {
+  //Logic goes here
 });
 
 module.exports = router;
