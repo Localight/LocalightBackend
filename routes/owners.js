803e4e0630184567236390674edaafc99c7d6d35
Aaron <torch2424@hotmail.com>
2015-07-21 09:59:20 -0700

    Added the skeleton for the owners route

--- routes/owners.js
+++ routes/owners.js
@@ -3,9 +3,34 @@ var express = require('express'),
     mongoose = require('mongoose'),
     owner = mongoose.model('owner');
 
-/* Owner routes */
-router.get('/', function(req, res, next) {
-  res.send('Respond with owner stuff');
+/* Join an Owner */
+router.post('/owners/join', function(req, res, next) {
+  //Logic goes here
+});
+
+/* Login an Owner */
+router.post('/owners/login', function(req, res, next) {
+  //Logic goes here
+});
+
+/* reset an Owner */
+router.post('/owners/reset', function(req, res, next) {
+  //Logic goes here
+});
+
+/* Get an Owner */
+router.get('/owners', function(req, res, next) {
+  //Logic goes here
+});
+
+/* Update an Owner */
+router.put('/owners', function(req, res, next) {
+  //Logic goes here
+});
+
+/* Remove an Owner */
+router.delete('/owners', function(req, res, next) {
+  //Logic goes here
 });
 
 module.exports = router;
