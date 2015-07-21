827a6d1a9889e5639b4f7cdc80ba3ea8d5104458
Aaron <torch2424@hotmail.com>
2015-07-21 09:44:35 -0700

    Created the skeleton for the user route

--- routes/users.js
+++ routes/users.js
@@ -3,9 +3,39 @@ var express = require('express'),
     mongoose = require('mongoose'),
     user = mongoose.model('user');
 
-/* User routes */
-router.get('/', function(req, res, next) {
-  res.send('Respond with user stuff');
+/* Join a user */
+router.post('/users/join', function(req, res, next) {
+  //Logic goes here
+});
+
+/* Get a user */
+router.get('/users', function(req, res, next) {
+  //Logic goes here
+});
+
+/* update a user */
+router.put('/users', function(req, res, next) {
+  //Logic goes here
+});
+
+/* remove a user */
+router.delete('/users', function(req, res, next) {
+  //Logic goes here
+});
+
+/* login a user */
+router.post('/users/login', function(req, res, next) {
+  //Logic goes here
+});
+
+/* signup a user through twilio */
+router.post('/users/twilio', function(req, res, next) {
+  //Logic goes here
+});
+
+/* reset a user */
+router.post('/users/reset', function(req, res, next) {
+  //Logic goes here
 });
 
 module.exports = router;
