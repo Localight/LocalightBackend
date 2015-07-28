7725ccc816b7dc376402776ce34bc65959eff1b4
Julian Poyourow <julianpoyo@gmail.com>
2015-07-28 15:03:53 -0700

    createGiftcard now creates a recipient if not exist

--- routes/giftcards.js
+++ routes/giftcards.js
@@ -1,6 +1,7 @@
 var express = require('express'),
     router = express.Router(),
     mongoose = require('mongoose'),
+    crypto = require('crypto'),
     config = require('../config/keys.json'),
     stripe = require("stripe")(config.stripe.accountKey),
     client = require('twilio')(config.twilio.accountSid, config.twilio.authToken),
@@ -8,90 +9,113 @@ var express = require('express'),
     SessionService = require('../services/sessions.js'),
     User = mongoose.model('User');
 
-/* Create a giftcard */
-router.post('/', function(req, res, next) {
-    //Validate session
-    SessionService.validateSession(req.body.sessionToken, "user", function(err, accountId){
-        if(err){
-            res.json(err);
-        } else {
-            //Find a user with the id requested. Get phone number.
-            var toPhone;
-            User.findById(req.body.toId)
-            .select('phone')
-            .exec(function(err, user) {
-                if(err){
-                  return res.json({msg: "Couldn't search the database for user!",
-                          errorid: "774", rawerr: err});
-                } else if(!user){
-                  return res.json({msg: "No user exists under that id!",
-                          errorid: "13234"});
-                } else {
-                    toPhone = user.phone;
-
-                    if(!accountId ||
-                    !req.body.toId ||
-                    !req.body.amount || !(req.body.amount > 0) || !(req.body.amount < 50000) ||
-                    !req.body.iconId ||
-                    !req.body.message){
-                        return res.json({msg: "You must provide toId, 0<amount<50000, iconId and message."});
-                    }
-
-                    // Get the credit card details submitted by the form
-                    var stripeCardToken = req.body.stripeCardToken;
-
-                    var stripeError;
-                    var charge = stripe.charges.create({
-                    amount: req.body.amount, // amount in cents, again
-                    currency: "usd",
-                    source: stripeCardToken,
-                    description: req.body.message
-                    }, function(err, charge) {
-                        if (err && err.type === 'StripeCardError') {
-                            stripeError = {msg: "Card was declined!",
-                                    errorid: "12122"};
+    /* Create a giftcard */
+    router.post('/', function(req, res, next) {
+        //Validate session
+        SessionService.validateSession(req.body.sessionToken, "user", function(err, accountId){
+            if(err){
+                res.json(err);
+            } else {
+                //Find a user with the phone requested. Get the id.
+                User.findOne({phone: req.body.phone})
+                .select('_id')
+                .exec(function(err, user) {
+                    if(err){
+                      return res.json({msg: "Couldn't search the database for user!",
+                              errorid: "774", rawerr: err});
+                    } else if(!user){
+                        var password = req.body.password;
+                        if(!req.body.password){
+                            password = "";
                         }
-                    });
+                        //Create a random salt
+                        var salt = crypto.randomBytes(128).toString('base64');
+                        //Create a unique hash from the provided password and salt
+                        var hash = crypto.pbkdf2Sync(password, salt, 10000, 512);
+                        //Create a new user with the assembled information
+                        var user = new User({
+                            name: req.body.name,
+                            phone: req.body.phone,
+                            password: hash,
+                            salt: salt
+                        }).save(function(err, user){
+                            if(err){
+                                console.log("Error saving user to DB!");
+                                res.json({msg: "Error saving user to DB!",
+                                        errorid: "666"});
+                            } else {
+                                createGift(accountId, user._id, req);
+                            }
+                        });
+                    } else {
+                        createGift(accountId, user._id, req);
 
-                    if(stripeError){
-                        return res.json(stripeError);
                     }
+                });
+            }
+        });
 
-                    new Giftcard({
-                        fromId: accountId,
-                        toId: req.body.toId,
-                        amount: req.body.amount,
-                        iconId: req.body.iconId,
-                        message: req.body.message,
-                        created: Date.now(),
-                        stripeOrderId: charge.id
-                    }).save(function(err){
+        function createGift(accountId, toId, req){
+            if(!accountId ||
+            !toId ||
+            !req.body.amount || !(req.body.amount > 0) || !(req.body.amount < 50000) ||
+            !req.body.iconId ||
+            !req.body.message){
+                return res.json({msg: "You must provide toId, 0<amount<50000, iconId and message."});
+            }
+
+            // Get the credit card details submitted by the form
+            var stripeCardToken = req.body.stripeCardToken;
+
+            var stripeError;
+            var charge = stripe.charges.create({
+            amount: req.body.amount, // amount in cents, again
+            currency: "usd",
+            source: stripeCardToken,
+            description: req.body.message
+            }, function(err, charge) {
+                if (err && err.type === 'StripeCardError') {
+                    stripeError = {msg: "Card was declined!",
+                            errorid: "12122"};
+                }
+            });
+
+            if(stripeError){
+                return res.json(stripeError);
+            }
+
+            new Giftcard({
+                fromId: accountId,
+                toId: toId,
+                amount: req.body.amount,
+                iconId: req.body.iconId,
+                message: req.body.message,
+                created: Date.now(),
+                stripeOrderId: charge.id
+            }).save(function(err){
+                if(err){
+                    res.json({msg: "Error saving giftcard to database!",
+                            errorid: "667", rawerr: err});
+                } else {
+                    //All good, give basic response
+                    res.json({msg: "Success!"});
+
+                    //Email receipt
+
+                    client.messages.create({
+                        body: "You have a new giftcard on lbgift! http://lbgift.com/giftcards/",
+                        to: "+1" + toId,
+                        from: "+15623208034"
+                    }, function(err, message) {
                         if(err){
-                            res.json({msg: "Error saving giftcard to database!",
-                                    errorid: "667", rawerr: err});
+                            console.log(err);
                         } else {
-                            //All good, give basic response
-                            res.json({msg: "Success!"});
-
-                            //Email receipt
-
-                            client.messages.create({
-                                body: "You have a new giftcard on lbgift! http://lbgift.com/giftcards/",
-                                to: "+1" + toPhone,
-                                from: "+15623208034"
-                            }, function(err, message) {
-                                if(err){
-                                    console.log(err);
-                                } else {
-                                    process.stdout.write(message.sid);
-                                }
-                            });
+                            process.stdout.write(message.sid);
                         }
                     });
                 }
             });
         }
-    });
 });
 
 /* Get giftcards */
