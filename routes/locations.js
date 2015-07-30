aa3b460329601e5ce9713466fe2de8af6891405d
Julian Poyourow <julianpoyo@gmail.com>
2015-07-29 18:02:44 -0700

    Added location creation

--- routes/locations.js
+++ routes/locations.js
@@ -1,36 +1,68 @@
 var express = require('express'),
     router = express.Router(),
     mongoose = require('mongoose'),
+    SessionService = require('../services/sessions.js'),
     Location = mongoose.model('Location');
 
 /* Create a Location */
 router.post('/', function(req, res, next) {
-  //Logic goes here
+    //Check if required was sent
+    if (!(req.body.name &&
+            req.body.triconKey &&
+            req.body.address1 &&
+            req.body.sessionToken)) {
+        return res.json({
+            msg: "You must provide all required fields!",
+            errorid: "994"
+        });
+    }
+    SessionService.validateSession(req.body.sessionToken, "owner", function(err, accountId) {
+        new Location({
+            name: req.body.name,
+            triconKey: req.body.triconKey,
+            address1: req.body.address1,
+            address2: req.body.address2,
+            city: req.body.city,
+            state: req.body.state,
+            zipcode: req.body.zipcode,
+            ownerId: accountId
+        }).save(function(err, location) {
+            if (err) {
+                console.log("Error saving location to DB!");
+                res.json({
+                    msg: "Error saving location to DB!",
+                    errorid: "666"
+                });
+            } else {
+                res.json({status: 200});
+            }
+        });
+    });
 });
 
 /* Get all Locations */
 router.get('/', function(req, res, next) {
-  //Logic goes here
+    //Logic goes here
 });
 
 /* Get a Location by id */
 router.get('/:id', function(req, res, next) {
-  //Logic goes here
+    //Logic goes here
 });
 
 /* Update a Location */
 router.put('/:id', function(req, res, next) {
-  //Logic goes here
+    //Logic goes here
 });
 
 /* Delete a Location */
 router.delete('/:id', function(req, res, next) {
-  //Logic goes here
+    //Logic goes here
 });
 
 /* Make a purchase at a location */
 router.post('/:id/spend', function(req, res, next) {
-  //Logic goes here
+    //Logic goes here
 });
 
 module.exports = router;
