5cb63bdf8a428761e03b9b4574bc4cde9e1e13c9
James Hall <greatwolf3d@gmail.com>
2015-08-03 16:53:19 -0700

    Finished Create GiftCard Test

--- tests/giftcard.route.test.js
+++ tests/giftcard.route.test.js
@@ -12,7 +12,7 @@ var should = require('should'),
  * Globals
  */
 
-var textBody, user1, user2, giftcard;
+var textBody, user1, user2, giftcardDump, giftcard;
 
 /**
  * Gitcard Routes Tests
@@ -26,43 +26,77 @@ describe('Giftcard Routes Tests', function() {
       user1 = new User({
          name: 'test user',
          email: 'test@test.com',
-         phone: '1112223333',
-         password:'password'
+         phone: '+11234567890',
+         password: 'password'
       });
 
       user2 = new User({
          name: 'test2 user2',
          email: 'test2@test.com',
-         phone: '1234567890',
-         password:'password'
+         phone: '+11234567890',
+         password: 'password'
       });
 
       textBody = {
-         From:user1.phone,
-         Body:'Gift'
+         From: user1.phone,
+         Body: 'Gift'
       };
 
-      giftcard = new Giftcard({
-         sessionToken: 'something',
+      giftcardDump = {
+         sessionToken: 'something', // need session token
          name: 'bob',
-         fromId: user1._id,
-         toId: user2._id,
+         phone: '1234567890',
          amount: 39393,
-         iconID: '232',
+         iconId: '232',
+         toId: user2._id,
          message: 'a gift for you!',
          stripeCardToken: 'ch_2h234'
-      }); // end giftcard
+      }; // end giftcard
 
       user2.save();
       user1.save(); // end user1 save
+      // make call to twilio and get session token.
       done();
    }); // end beforeEach
    // Create a way for us to test create of giftcard based on presence of session token or not.
 
    /**
-    * Create A gifcard - Test1
+    * Session Token
     */
-   it('should be able to get a session token given the phone number is correct', function(done) {
+   // it('should be able to get a session token given the phone number is correct', function(done) {
+   //    agent.post('/users/twilio')
+   //       .send(textBody)
+   //       .expect(200)
+   //       .end(function(signinErr, signinRes) {
+   //          // Handle Sign-In error
+   //
+   //          if (signinErr) {
+   //             console.log('this is the signin error' + JSON.stringify(signinErr.body));
+   //             done(signinErr);
+   //          }
+   //
+   //          if (signinRes) {
+   //             console.log('this is the sigin Response' + JSON.stringify(signinRes.text));
+   //             var holder = signinRes.text;
+   //             console.log(holder);
+   //
+   //             var something = holder.indexOf('e/');
+   //             var anotherThing = holder.substr(56, 96);
+   //             console.log('the value of the parsed string ' + anotherThing);
+   //             console.log(giftcard);
+   //             giftcardDump.sessionToken = anotherThing;
+   //
+   //             console.log(giftcardDump);
+   //             done(signinRes);
+   //          }
+   //          done();
+   //       });
+   // });
+
+   /**
+    * Create Gift Card
+    */
+   it('should be able to save the giftcard successfully,  given correct parameter', function(done) {
       agent.post('/users/twilio')
          .send(textBody)
          .expect(200)
@@ -70,30 +104,46 @@ describe('Giftcard Routes Tests', function() {
             // Handle Sign-In error
 
             if (signinErr) {
-               console.log('this is the signin error'+JSON.stringify(signinErr.body));
+               console.log('this is the signin error' + JSON.stringify(signinErr.body));
                done(signinErr);
             }
+            var holder = signinRes.text;
+            // console.log(holder);
+            var anotherThing = holder.substr(56, 96);
+            // console.log('the value of the parsed string ' + anotherThing);
+            // console.log(giftcard);
+            giftcardDump.sessionToken = anotherThing;
+            agent.post('/giftcards')
+               .send(giftcardDump)
+               .expect(201)
+               .end(function(giftcardSaveErr, giftcardSaveRes) {
+                  if (giftcardSaveErr) {
+                     console.log('got an error trying to save giftcard' + giftcardSaveErr);
+                     done(giftcardSaveErr);
+                  }
+                  // console.log('results of saving' + JSON.stringify(giftcardSaveRes));
 
-            if(signinRes){
-               console.log('this is the sigin Response'+JSON.stringify(signinRes.body));
-               done(signinRes);
-            }
-            done();
+                  should.not.exist(giftcardSaveErr);
+                  done();
+               });
          });
    });
 
-   it('should be able to save the giftcard successfully,  given correct parameter', function(done) {
-      agent.post('/giftcards') // TODO: add in url
+
+   /**
+    * Get Gift Cards
+    */
+   it('should be able to give back a list of giftcard, given correct parameter', function(done) {
+      agent.post('') // TODO: add in url
          .send(credentials)
          .expect(200)
          .end(function(signinErr, signinRes) {
             // Handle Sigin Error
             if (signinErr) {
-               console.log('You had an error Signining in: ' + JSON.stringify(signinErr));
-               console.log('You got a successfully logged in:' + JSON.stringify(signinRes.body));
+               console.log('You had an error Signin in: ' + signinErr);
+               console.log('You got a successfully logged in:' + signinRes.body);
                done(signinErr);
             }
-
             // save a new giftcard
             agent.post('/') // need the long rougte for this to work.
                .send(Giftcard)
@@ -111,9 +161,9 @@ describe('Giftcard Routes Tests', function() {
    });
 
    /**
-    * Get A gifcard - Test1
+    * Get A Gift Card
     */
-   it('should be able to save the giftcard successfully,  given correct parameter', function(done) {
+   it('should be able to give back a giftcard, given correct parameter', function(done) {
       agent.post('') // TODO: add in url
          .send(credentials)
          .expect(200)
