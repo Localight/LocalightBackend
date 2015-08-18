f005f2169cd3eb1241c043ea18e75545bb0dedfc
Julian Poyourow <julianpoyo@gmail.com>
2015-08-18 10:22:55 -0700

    Removed index page

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    res.status(404).send("Not found");
 });
 
 module.exports = router;
