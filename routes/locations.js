65424995b07fae795fc9a7bcffe29faee2e90c73
Julian Poyourow <julianpoyo@gmail.com>
2015-10-12 16:42:01 -0700

    Auto formatted locations

--- routes/locations.js
+++ routes/locations.js
@@ -27,11 +27,15 @@ router.post('/', function(req, res) {
     });
 
     function createCode() {
-        giftCode = Math.floor(Math.random()*90000) + 10000;
+        giftCode = Math.floor(Math.random() * 90000) + 10000;
 
         //Check if an owner with that email already exists
         Location.findOne({
-                $or:[ {'ownerCode': giftCode}, {'subs.subCode': giftCode}]
+                $or: [{
+                    'ownerCode': giftCode
+                }, {
+                    'subs.subCode': giftCode
+                }]
             })
             .select('_id')
             .exec(function(err, owner) {
@@ -43,7 +47,7 @@ router.post('/', function(req, res) {
             });
     }
 
-    function createLocation(){
+    function createLocation() {
         new Location({
             name: req.body.name,
             triconKey: req.body.triconKey,
@@ -116,7 +120,7 @@ router.get('/code', function(req, res) {
                 return res.status(500).json({
                     msg: "Couldn't query the database for locations!"
                 });
-            } else if(location){
+            } else if (location) {
                 res.status(200).json(locations);
             } else {
                 Location.findOne({
@@ -128,10 +132,10 @@ router.get('/code', function(req, res) {
                             return res.status(500).json({
                                 msg: "Couldn't query the database for locations!"
                             });
-                        } else if(location){
+                        } else if (location) {
                             var subId = "";
-                            for(sub in location.subs){
-                                if(sub.subCode == req.query.code){
+                            for (sub in location.subs) {
+                                if (sub.subCode == req.query.code) {
                                     subId = sub.subId;
                                 }
                             }
@@ -228,112 +232,114 @@ router.post('/:id/spend', function(req, res, next) {
             res.json(err);
         } else {
             Location.findOne({
-                _id: req.params.id,
-                triconKey: req.body.triconKey
-            })
-            .select('_id triconKey')
-            .exec(function(err, location){
-                if (err) {
-                    return res.status(500).json({
-                        msg: "Couldn't search the database for location!"
-                    });
-                } else if(!location) {
-                    return res.status(404).json({
-                        msg: "Invalid ID or Tricon!"
-                    });
-                } else {
-                    //Find all valid giftcards for the user
-                    Giftcard.find({
-                            toId: accountId,
-                            amount: {
-                                $gt: 0
-                            },
-                            sent: true
-                        })
-                        .select('_id amount')
-                        .exec(function(err, giftcards) {
-                            if (err) {
-                                return res.status(500).json({
-                                    msg: "Couldn't search the database for giftcard!"
-                                });
-                            } else {
-                                //Total up the user's giftcards for balance
-                                var total = 0;
-                                for (var i = 0; i < giftcards.length; i++) {
-                                    total += giftcards[i].amount;
-                                }
-                                //Check if desired chargeAmount is greater than total balance
-                                var chargeAmt = req.body.amount;
-                                if (chargeAmt > total) {
-                                    res.status(402).json({
-                                        msg: "Not enough funds."
+                    _id: req.params.id,
+                    triconKey: req.body.triconKey
+                })
+                .select('_id triconKey')
+                .exec(function(err, location) {
+                    if (err) {
+                        return res.status(500).json({
+                            msg: "Couldn't search the database for location!"
+                        });
+                    } else if (!location) {
+                        return res.status(404).json({
+                            msg: "Invalid ID or Tricon!"
+                        });
+                    } else {
+                        //Find all valid giftcards for the user
+                        Giftcard.find({
+                                toId: accountId,
+                                amount: {
+                                    $gt: 0
+                                },
+                                sent: true
+                            })
+                            .select('_id amount')
+                            .exec(function(err, giftcards) {
+                                if (err) {
+                                    return res.status(500).json({
+                                        msg: "Couldn't search the database for giftcard!"
                                     });
                                 } else {
-                                    new Transaction({
-                                        userId: accountId,
-                                        locationId: req.params.id,
-                                        amount: chargeAmt
-                                    }).save(function(err, transaction) {
-                                        if (err) {
-                                            console.log("Error saving charge to DB!");
-                                            res.status(500).json({
-                                                msg: "Error saving charge to DB!"
-                                            });
-                                        } else {
-                                            //Run until chargeAmount is satisfied
-                                            var i = 0;
-                                            while (chargeAmt > 0) {
-                                                //Store the deducted giftcard amount
-                                                var newGcAmt;
-                                                if (chargeAmt > giftcards[i].amount) {
-                                                    //chargeAmount greater than giftcard, 0 the giftcard
-                                                    newGcAmt = 0;
-                                                } else {
-                                                    //Deduct the chargeAmount from giftcard
-                                                    newGcAmt = giftcards[i].amount - chargeAmt;
-                                                }
-                                                //Prepare the deducted card balance
-                                                var updateGiftcard = {
-                                                        $set: {
-                                                            amount: newGcAmt
-                                                        }
+                                    //Total up the user's giftcards for balance
+                                    var total = 0;
+                                    for (var i = 0; i < giftcards.length; i++) {
+                                        total += giftcards[i].amount;
+                                    }
+                                    //Check if desired chargeAmount is greater than total balance
+                                    var chargeAmt = req.body.amount;
+                                    if (chargeAmt > total) {
+                                        res.status(402).json({
+                                            msg: "Not enough funds."
+                                        });
+                                    } else {
+                                        new Transaction({
+                                            userId: accountId,
+                                            locationId: req.params.id,
+                                            amount: chargeAmt
+                                        }).save(function(err, transaction) {
+                                            if (err) {
+                                                console.log("Error saving charge to DB!");
+                                                res.status(500).json({
+                                                    msg: "Error saving charge to DB!"
+                                                });
+                                            } else {
+                                                //Run until chargeAmount is satisfied
+                                                var i = 0;
+                                                while (chargeAmt > 0) {
+                                                    //Store the deducted giftcard amount
+                                                    var newGcAmt;
+                                                    if (chargeAmt > giftcards[i].amount) {
+                                                        //chargeAmount greater than giftcard, 0 the giftcard
+                                                        newGcAmt = 0;
+                                                    } else {
+                                                        //Deduct the chargeAmount from giftcard
+                                                        newGcAmt = giftcards[i].amount - chargeAmt;
                                                     }
-                                                    //Deduct from the card balance
-                                                Giftcard.update({
-                                                        _id: giftcards[i]._id
-                                                    }, updateGiftcard)
-                                                    .exec(function(err, location) {
-                                                        if (err) {
-                                                            //Prepare the error
-                                                            var transactionError = {
+                                                    //Prepare the deducted card balance
+                                                    var updateGiftcard = {
+                                                            $set: {
+                                                                amount: newGcAmt
+                                                            }
+                                                        }
+                                                        //Deduct from the card balance
+                                                    Giftcard.update({
+                                                            _id: giftcards[i]._id
+                                                        }, updateGiftcard)
+                                                        .exec(function(err, location) {
+                                                            if (err) {
+                                                                //Prepare the error
+                                                                var transactionError = {
                                                                     $push: {
-                                                                        errs: {err: err}
+                                                                        errs: {
+                                                                            err: err
+                                                                        }
                                                                     }
                                                                 }
-                                                            Transaction.update({
-                                                                    _id: transaction.id
-                                                                }, transactionError)
-                                                                .exec(function(err, location) {
-                                                                    if (err) {
-                                                                        console.log("FATAL error logging giftcard deduction transaction error!")
-                                                                    }
-                                                                });
-                                                        }
-                                                    });
-                                                //Subtract from the remaining amount to be charged
-                                                chargeAmt = chargeAmt - giftcards[i].amount;
-                                                i++;
+                                                                Transaction.update({
+                                                                        _id: transaction.id
+                                                                    }, transactionError)
+                                                                    .exec(function(err, location) {
+                                                                        if (err) {
+                                                                            console.log("FATAL error logging giftcard deduction transaction error!")
+                                                                        }
+                                                                    });
+                                                            }
+                                                        });
+                                                    //Subtract from the remaining amount to be charged
+                                                    chargeAmt = chargeAmt - giftcards[i].amount;
+                                                    i++;
+                                                }
+                                                res.status(200).json({
+                                                    msg: "Charge was completed!"
+                                                });
                                             }
-                                            res.status(200).json({
-                                                msg: "Charge was completed!"
-                                            });
-                                        }
-                                    });
+                                        });
+                                    }
                                 }
-                            }
-                        });
-                }
-            });
+                            });
+                    }
+                });
         }
     });
 });
