6ad8616c8ed659a3a6a9e1f9699b7005ba6623a7
Julian Poyourow <julianpoyo@gmail.com>
2015-10-23 10:51:37 -0700

    Added shorturl service

---
+++ services/shortURL.js
@@ -0,0 +1,21 @@
+var request = require('request');
+
+//Checks if a token exists, and returns the corrosponding accountId
+exports.create = function(url, callback) {
+    request.post({
+            headers: {
+                'content-type': 'application/x-www-form-urlencoded'
+            },
+            url: 'http://s.julianjp.com/submit.php',
+            body: "url=" + url
+        },
+        function(err, response, body) {
+            if(err){
+                console.log("problem accessing url shortner");
+            } else {
+                callback(body);
+            }
+        });
+
+
+};
